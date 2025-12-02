f166ec7f3088832a1cec69ee5b79898043f1cf12
codewithelie <me@elyh.fr>
2025-12-02 11:57:36 +0100

    Ajout des routes pour la gestion des projets : création, affichage, édition, mise à jour et suppression des projets dans le ProjectController.

--- config/routes.php
+++ config/routes.php
@@ -5,13 +5,25 @@
 $router->get('/', 'HomeController@index');
 
 /* Project Routes */
+/* index */
 $router->get('/projects', 'ProjectController@index');
+
+/* create form */
 $router->get('/projects/create', 'ProjectController@create');
+
+/* store */
 $router->post('/projects', 'ProjectController@store');
 
+/* show */
 $router->get('/projects/{id}/show', 'ProjectController@show');
+
+/* edit form */
 $router->get('/projects/{id}/edit', 'ProjectController@edit');
+
+/* update */
 $router->post('/projects/{id}/update', 'ProjectController@update');
+
+/* delete */
 $router->post('/projects/{id}/delete', 'ProjectController@destroy');
 
 
