c4abf0bbb0ee88d9cc2ed01cb36780d6e76477d8
codewithelie <me@elyh.fr>
2025-12-04 12:17:45 +0100

    Suppression de la méthode renderOld dans la classe View et ajout d'un script Tailwind CSS dans le fichier main.twig.

--- app/Core/View.php
+++ app/Core/View.php
@@ -32,28 +32,4 @@ class View {
         self::init();
         echo self::$twig->render($viewName . '.twig', $data);
     }
-
-    public static function renderOld(string $viewName ,array $data = []) {
-        $viewFile = __DIR__ . '/../Views/' . $viewName . '.php';
-        
-        if (!file_exists($viewFile)) {
-            throw new \Exception("Vue non trouvé : $viewFile");
-        }
-
-        extract($data); 
-
-        ob_start();
-        include $viewFile;
-        $content = ob_get_clean();
-
-        $layoutPath = __DIR__ . '/../Views/layout/main.php';
-
-        $user = Auth::user();
-        $notifications = Notification::get();  
-
-        ob_start();
-        require $layoutPath;
-        $layout = ob_get_clean();
-        echo $layout;
-    }
 }
